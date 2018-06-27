9b65df54c818c73cc20cb23874c6edf86ab98a5a
Axel MYRE <axel.myre@epitech.eu>
2018-06-27 19:06:30 +0200

    Add notifier

--- public/app/common/mvNotifier.js
+++ public/app/common/mvNotifier.js
@@ -1,5 +1,10 @@
 angular.module('app').value('mvToaster', toastr); //what ? 
 
 angular.module('app').factory('mvNotifier', function(mvToastr) {
-    
+    return {
+        notify : function(msg) {
+            mvToaster.success(msg);
+            console.log(msg);
+        }       
+    }
 })
